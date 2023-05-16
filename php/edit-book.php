c0e97a1b2d86739f4ac5b06cfe91784078fb1593
lediona` <lediona.zubaku@.gmail.com`>
2023-05-16 20:03:03 +0200

    admins update of the book cover and the file

--- php/edit-book.php
+++ php/edit-book.php
@@ -72,37 +72,28 @@ if(isset($_SESSION['user_id']) &&
         $ms = "id=$id&error";
         is_empty($category, $text, $location, $ms, "");
 
+        #if the admin tries to update the book cover    
 
-        #simple from Validation
-        if(empty($name)){
-            $em = "The author name is required";
-            header("Location: ../edit-author.php?error=$em&id=$id");
-            exit;
-        }else{
-            #UPDATE the database
-            $sql  = "UPDATE authors  
-                    SET name=?
-                    WHERE id=?";
-            $stmt = $conn-> prepare($sql);
-            $res  = $stmt-> execute([$name, $id]);
-            /*
-                if there is no error while 
-                inserting the data
-            */ 
-            if($res){
-                # Success message
-                $sm = "Successfully updated !";
-                header("Location: ../edit-author.php?success=$sm&id=$id");
-                exit;
+         if(!empty($_FILES['book_cover']['name'])){
+                #if thw admin tries to update both
+
+            if(!empty($_FILES['book_cover']['name'])){
+                #update both here
             }else{
-                # Error message
-                $em = "Unknown Error Occurred!";
-                header("Location: ../edit-author.php?error=$em&id=$id");
-                exit;
+                #update just book cover
             }
-        }        
-
-   
+        
+           
+         }
+        #if the admin tries to update just the file
+        else if(!empty($_FILES['file']['name'])){
+            #update just the file
+        }else{
+            #update just the data
+            $sql = "UPDATE books
+                    SET title =?
+                         author_id=?"
+        }
 }else{
     header("Location: ../admin.php");
     exit;
