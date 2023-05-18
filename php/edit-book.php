3055bbd1d2423d38261c00691fcfaa641d91e64f
lediona` <lediona.zubaku@.gmail.com`>
2023-05-18 15:19:53 +0200

    book cover edit

--- php/edit-book.php
+++ php/edit-book.php
@@ -214,9 +214,9 @@ if(isset($_SESSION['user_id']) &&
                 #update just the file
                      
             # book cover Uploading
-            $allowed_image_exs = array("jpg", "jpeg", "png");
-            $path = "cover";
-            $book_cover = upload_file($_FILES['book_cover'], $allowed_image_exs, $path);
+            $allowed_file_exs = array("pdf", "docx", "pptx");
+            $path = "files";
+            $files = upload_file($_FILES['file'], $allowed_file_exs, $path);
 
             /*
             If error occurred while 
