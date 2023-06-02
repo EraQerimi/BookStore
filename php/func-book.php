50fcc47ce068dfa2daaa023dc81fb15260000947
Your Name <you@example.com>
2023-06-02 17:56:34 +0200

    get books by category

--- php/func-book.php
+++ php/func-book.php
@@ -49,3 +49,19 @@ function search_books($con, $key){
 
    return $books;
 }
+
+# get books by category
+
+function get_books_by_category($con, $id){
+   $sql  = "SELECT * FROM books WHERE  category_id=?";
+   $stmt = $con->prepare($sql);
+   $stmt->execute([$id]);
+
+   if ($stmt->rowCount() > 0) {
+   	  $book = $stmt->fetch();
+   }else {
+      $book = 0;
+   }
+
+   return $book;
+}
