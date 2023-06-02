b053c28a9588a6bca899f1b6ae27c9cc9641b3e3
Your Name <you@example.com>
2023-06-02 12:56:08 +0200

    new file added

--- search.php
+++ search.php
@@ -44,7 +44,7 @@ $key = $_GET['key'];
     <div class="conainer">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Online Book Store</a>
+                <a class="navbar-brand" href="index.php">Online Book Store</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
