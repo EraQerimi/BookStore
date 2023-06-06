389dc7279c09f45bceedac19bb60e50ee2ead153
era qerimi <eq58315@ubt-uni.net>
2023-06-06 18:06:17 +0200

    new lines added

--- edit-book.php
+++ edit-book.php
@@ -1,242 +1,238 @@
-<?php
+<?php  
 session_start();
 
-#if the admin is logged in
-if(isset($_SESSION['user_id']) &&
-   isset($_SESSION['user_email'])){
-     #if book id is not set
-     if(!isset($_GET['id'])){
-        #redirect to admin.php page
+# If the admin is logged in
+if (isset($_SESSION['user_id']) &&
+    isset($_SESSION['user_email'])) {
+
+	# If book ID is not set
+	if (!isset($_GET['id'])) {
+		#Redirect to admin.php page
         header("Location: admin.php");
         exit;
-    }
+	}
 
-    $id = $_GET['id'];
+	$id = $_GET['id'];
 
+	# Database Connection File
+	include "db_conn.php";
 
-    #Database connection file
-    include "db_conn.php";
-    
-    # Book helper function
-    include "php/func-book.php";
+	# Book helper function
+	include "php/func-book.php";
     $book = get_book($conn, $id);
-
-    #If the ID is invalid
-    if($book == 0){
-        #Redirect to admin.php page
+    
+    # If the ID is invalid
+    if ($book == 0) {
+    	#Redirect to admin.php page
         header("Location: admin.php");
         exit;
     }
 
     # Category helper function
-    include "php/func-category.php";
+	include "php/func-category.php";
     $categories = get_all_categories($conn);
 
-    # Author helper function
-    include "php/func-author.php";
+    # author helper function
+	include "php/func-author.php";
     $authors = get_all_author($conn);
 
-    
-    
 ?>
-    
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Book</title>
-     <!-- bootstrap cdn -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Edit Book</title>
 
-    <!-- bootstrap js cdn-->
-    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js "></script>
+    <!-- bootstrap 5 CDN-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <!-- bootstrap 5 Js bundle CDN-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    <div class="conainer">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="admin.php">Admin</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="index.php">Store</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="add-book.php">Add Book</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="add-category.php">Add Category</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="add-author.php">Add Author</a>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-     <form action ="php/edit-book.php"
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="admin.php">Admin</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" 
+		         id="navbarSupportedContent">
+		      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             aria-current="page" 
+		             href="index.php">Store</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             href="add-book.php">Add Book</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             href="add-category.php">Add Category</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             href="add-author.php">Add Author</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" 
+		             href="logout.php">Logout</a>
+		        </li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+     <form action="php/edit-book.php"
            method="post"
-           enctype="multipart/form-data"
+           enctype="multipart/form-data" 
            class="shadow p-4 rounded mt-5"
            style="width: 90%; max-width: 50rem;">
 
-        <h1 class="text-center pb-5 display-4 fs-3">
-            Edit Book
-        </h1>
-        <?php if(isset($_GET['error'])) {?>
-         <div class="alert alert-danger" role="alert">
-            <?=htmlspecialchars($_GET['error']);?>
-         </div>
-        <?php } ?>
-        <?php if(isset($_GET['success'])) {?>
-         <div class="alert alert-success" role="alert">
-            <?=htmlspecialchars($_GET['success']);?>
-         </div>
-        <?php } ?>
-        <div class="mb-3">
-            <label class="form-label">
-                Book Title
-                </label>
-            <input type="text" 
-                    hidden
-                    value="<?=$book['id']?>"
-                    name="book_id">
-                    
-            <input type="text" 
-                    class="form-control"
-                    value="<?=$book['title']?>"
-                    name="book_title">
-        </div>
+     	<h1 class="text-center pb-5 display-4 fs-3">
+     		Edit Book
+     	</h1>
+     	<?php if (isset($_GET['error'])) { ?>
+          <div class="alert alert-danger" role="alert">
+			  <?=htmlspecialchars($_GET['error']); ?>
+		  </div>
+		<?php } ?>
+		<?php if (isset($_GET['success'])) { ?>
+          <div class="alert alert-success" role="alert">
+			  <?=htmlspecialchars($_GET['success']); ?>
+		  </div>
+		<?php } ?>
+     	<div class="mb-3">
+		    <label class="form-label">
+		           Book Title
+		           </label>
+		    <input type="text" 
+		           hidden
+		           value="<?=$book['id']?>" 
+		           name="book_id">
 
-        <div class="mb-3">
-            <label class="form-label">
-                Book Description
-                </label>
-            <input type="text" 
-                    class="form-control"
-                    value="<?=$book['description']?>"
-                    name="book_description">
-                    
-        </div>
+		    <input type="text" 
+		           class="form-control"
+		           value="<?=$book['title']?>" 
+		           name="book_title">
+		</div>
 
-        <div class="mb-3">
-            <label class="form-label">
-                Book Author
-                </label>
-                    <select name="book_author"
-                     class="form-control">
-                    <option value="0">
-                     Select author 
-                    </option>
-                 <?php 
+		<div class="mb-3">
+		    <label class="form-label">
+		           Book Description
+		           </label>
+		    <input type="text" 
+		           class="form-control" 
+		           value="<?=$book['description']?>"
+		           name="book_description">
+		</div>
 
-                 if($authors == 0){
-                    #Do nothing
-                 }else{ 
-                 foreach ($authors as $author) { 
-                    if($book['author_id'] == $author['id'])  {
-                 ?> 
-                <option
-                      selected
-                      value="<?=$author['id']?>">
-                      <?=$author['name']?>
-                </option>
-                <?php  } else{  ?>
-                     <option
-                    value="<?=$author['id']?>">
-                     <?=$author['name']?>
-                    </option>
-              <?php  } }} ?>          
-             </select>
-        </div>
+		<div class="mb-3">
+		    <label class="form-label">
+		           Book Author
+		           </label>
+		    <select name="book_author"
+		            class="form-control">
+		    	    <option value="0">
+		    	    	Select author
+		    	    </option>
+		    	    <?php 
+                    if ($authors == 0) {
+                    	# Do nothing!
+                    }else{
+		    	    foreach ($authors as $author) { 
+		    	    	if ($book['author_id'] == $author['id']) { ?>
+		    	    	<option 
+		    	    	  selected
+		    	    	  value="<?=$author['id']?>">
+		    	    	  <?=$author['name']?>
+		    	        </option>
+		    	        <?php }else{ ?>
+						<option 
+							value="<?=$author['id']?>">
+							<?=$author['name']?>
+						</option>
+		    	   <?php }} } ?>
+		    </select>
+		</div>
 
-        <div class="mb-3">
-            <label class="form-label">
-                Book Category
-                </label>
-             <select name="book_category"
-                     class="form-control">
-                    <option value="0">
-                     Select category 
-                    </option>
-                 <?php 
+		<div class="mb-3">
+		    <label class="form-label">
+		           Book Category
+		           </label>
+		    <select name="book_category"
+		            class="form-control">
+		    	    <option value="0">
+		    	    	Select category
+		    	    </option>
+		    	    <?php 
+                    if ($categories == 0) {
+                    	# Do nothing!
+                    }else{
+		    	    foreach ($categories as $category) { 
+		    	    	if ($book['category_id'] == $category['id']) { ?>
+		    	    	<option 
+		    	    	  selected
+		    	    	  value="<?=$category['id']?>">
+		    	    	  <?=$category['name']?>
+		    	        </option>
+		    	        <?php }else{ ?>
+						<option 
+							value="<?=$category['id']?>">
+							<?=$category['name']?>
+						</option>
+		    	   <?php }} } ?>
+		    </select>
+		</div>
 
-                 if($categories == 0){
-                    #Do nothing
-                 }else{ 
-                    foreach ($categories as $category) { 
-                        if($book['category_id'] == $category['id'])  {
-                     ?> 
-                    <option
-                          selected
-                          value="<?=$category['id']?>">
-                          <?=$category['name']?>
-                    </option>
-                    <?php  } else{  ?>
-                         <option
-                        value="<?=$category['id']?>">
-                         <?=$category['name']?>
-                        </option>
-                  <?php  } }} ?>         
-             </select>       
-                    
-        </div>
+		<div class="mb-3">
+		    <label class="form-label">
+		           Book Cover
+		           </label>
+		    <input type="file" 
+		           class="form-control" 
+		           name="book_cover">
 
-        <div class="mb-3">
-            <label class="form-label">
-                Book Cover
-                </label>
-            <input type="file" 
-                    class="form-control"
-                    name="book_cover">
+		     <input type="text" 
+		           hidden
+		           value="<?=$book['cover']?>" 
+		           name="current_cover">
 
-                    <input type="text" 
-                    hidden
-                    value="<?=$book['cover']?>"
-                    name="current_cover">
+		    <a href="uploads/cover/<?=$book['cover']?>"
+		       class="link-dark">Current Cover</a>
+		</div>
 
-                    <a href="uploads/cover/<?=$book['cover']?>" 
-                    class="link-dark">Current Cover</a>
-                    
-        </div>
+		<div class="mb-3">
+		    <label class="form-label">
+		           File
+		           </label>
+		    <input type="file" 
+		           class="form-control" 
+		           name="file">
 
-        <div class="mb-3">
-            <label class="form-label">
-                File
-                </label>
-            <input type="file" 
-                    class="form-control"
-                    name="file">
-                    <input type="text" 
-                    hidden
-                    value="<?=$book['file']?>"
-                    name="current_file"> 
-                    
-                    <a href="uploads/files/<?=$book['file']?>" 
-                    class="link-dark">Current File</a>
-                    
-                    
-        </div>
+		     <input type="text" 
+		           hidden
+		           value="<?=$book['file']?>" 
+		           name="current_file">
 
+		    <a href="uploads/files/<?=$book['file']?>"
+		       class="link-dark">Current File</a>
+		</div>
 
-        <button type="submit" 
-               class="btn btn-primary">
-               Update</button>
-      
+	    <button type="submit" 
+	            class="btn btn-primary">
+	            Update</button>
      </form>
-    </div>
+	</div>
 </body>
 </html>
 
 <?php }else{
-    header("Location: login.php");
-    exit;
+  header("Location: login.php");
+  exit;
 } ?>
